d52e059fc2af56d86006a384a9cbe6a8d23080af
haojun-mah <mahhaojun03@gmail.com>
2025-11-29 13:01:44 +0800

    feature: orchestrator

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+from ingestion.chunk_generation.title_chunker import title_chunker
+from ingestion.parser.parse_html import parse_html
+
+TARGET_FILE_LOCATION = "sec-edgar-filings/MSFT/8-K"
+
 def main():
-    print("Hello from rag!")
+    """Main orchestrator for Agentic Rag workflow"""
+   
+    folder = TARGET_FILE_LOCATION
+
+    parsed_elements = parse_html(folder)
+    if not parsed_elements:
+        print(f"No elements were parsed from {folder}. Nothing to chunk.")
+        return
+
+    print(f"\nParsed {len(parsed_elements)} elements from {folder}")
+
+    chunked_elements = title_chunker(parsed_elements)
+    print(f"\nGenerated {len(chunked_elements)} chunks from parsed elements.")
+    
+    
+    
+    
+    
+    
 
 
 if __name__ == "__main__":
